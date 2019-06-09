23c75e317e55c0add7f6f86ad2359c3386286155
ali-mayun <46866865+ali-mayun@users.noreply.github.com>
2019-06-10 00:31:23 +0800

    20190610dubbo hello world

---
+++ gmall-interface/src/main/java/com/ty/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.ty.service;
+
+import com.ty.bean.UserAddress;
+
+import java.util.List;
+
+public interface OrderService {
+	
+	/**
+	 * 初始化订单
+	 * @param userId
+	 */
+	public List<UserAddress> initOrder(String userId);
+
+}
---
+++ user-service-providor/src/main/java/com/ty/service/impl/UserServiceStub.java
@@ -0,0 +1,34 @@
+package com.ty.service.impl;
+
+import com.ty.bean.UserAddress;
+import com.ty.service.UserService;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+public class UserServiceStub implements UserService {
+	
+	private final UserService userService;
+	
+
+	/**
+	 * 传入的是userService远程的代理对象
+	 * @param userService
+	 */
+	public UserServiceStub(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+
+
+	@Override
+	public List<UserAddress> getUserAddressList(String userId) {
+		// TODO Auto-generated method stub
+		System.out.println("UserServiceStub.....");
+		if(!StringUtils.isEmpty(userId)) {
+			return userService.getUserAddressList(userId);
+		}
+		return null;
+	}
+
+}
